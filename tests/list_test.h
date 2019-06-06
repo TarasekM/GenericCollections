d7ed5b1dd50920b31981091664e5bf2792090f2a
TarasekM <rezkth@gmail.com>
2019-06-06 19:51:56 +0200

    Add test for more methods

--- tests/list_test.h
+++ tests/list_test.h
@@ -20,6 +20,11 @@ public:
         testGetAtAndSetAt();
         testIndexOf();
         testLastIndexOf();
+        testLastIndexOfMaxRange();
+        testLastIndexInRangeOf();
+        testRemoveAt();
+        testRemoveRange();
+        testRemoveAll();
     }
 
     void testConstructor(){
@@ -46,6 +51,13 @@ public:
     void testGetAtAndSetAt(){
         resetList();
         list.setAt(0, third_person);
+        try {
+            list.getAt(10);
+        }catch (std::string &Exception){
+            //TODO should make Exception class
+            assert(Exception == "Index out of bounds");
+        }
+
         assert(list.getAt(0).getName() == third_person.getName());
 
     }
@@ -64,6 +76,55 @@ public:
         resetList();
         list.add(first_person);
         assert(list.lastIndexOf(first_person) == 2);
+        assert(list.lastIndexOf(third_person) == -1);
+    }
+
+    void testLastIndexOfMaxRange(){
+        resetList();
+        list.add(first_person);
+        assert(list.lastIndexOf(first_person, 1) == 0);
+        assert(list.lastIndexOf(third_person) == -1);
+        assert(list.lastIndexOf(first_person, 10) == -1);
+    }
+
+    void testLastIndexInRangeOf(){
+        resetList();
+        list.add(third_person);
+        list.add(first_person);
+        list.add(first_person);
+        assert(list.lastIndexInRangeOf(first_person, 2, 4) == 4);
+        assert(list.lastIndexInRangeOf(first_person, 0, 2) == 0);
+        assert(list.lastIndexInRangeOf(first_person, 10, 15) == -1);
+        assert(list.lastIndexInRangeOf(first_person, 1, 2) == -1);
+        assert(list.lastIndexInRangeOf(first_person, 3, 2) == -1);
+    }
+
+    void testRemoveAt(){
+        resetList();
+        list.add(first_person);
+        list.removeAt(0);
+        assert(list.length() == 2);
+        assert(list.indexOf(second_person) == 0);
+        assert(list.indexOf(first_person) == 1);
+    }
+
+    void testRemoveRange(){
+        resetList();
+        list.add(third_person);
+        list.removeRange(0, 1);
+        assert(list.length() == 1);
+        assert(list.indexOf(first_person) == -1);
+        assert(list.indexOf(second_person) == -1);
+        assert(list.indexOf(third_person) == 0);
+    }
+
+    void testRemoveAll(){
+        resetList();
+        list.add(first_person);
+        list.removeAll(first_person);
+        assert(list.length() == 1);
+        assert(list.indexOf(first_person) == -1);
+        assert(list.indexOf(second_person) == 0);
     }
 
     void resetList(){
