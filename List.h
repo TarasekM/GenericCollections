a286cf8c7e85e8403a5746a5b179868a7f790d09
TarasekM <rezkth@gmail.com>
2019-06-06 19:52:21 +0200

    Add more methods

--- List.h
+++ List.h
@@ -1,6 +1,8 @@
 #ifndef QUEUE_LIST_H
 #define QUEUE_LIST_H
 
+#include <cassert>
+#include <iostream>
 #include "ICollection.h"
 #include "Iterator.h"
 
@@ -9,6 +11,8 @@ template <typename T> class List;
 template <typename T>
 class List : public ICollection<T>{
 private:
+    const std::string IndexOutOfBoundsException = "Index out of bounds";
+
     void addFromCollection(ICollection<T> &collection){
         Iterator<T> *iterator = collection.getIterator();
         while (iterator->moveNext()){
@@ -17,11 +21,11 @@ private:
     }
 public:
     List<T>() = default;
-    ~List() = default;
-
     explicit List<T>(ICollection<T> &collection){
         addFromCollection(collection);
     }
+    ~List() = default;
+
 
     void addCollection(ICollection<T> &collection){
         addFromCollection(collection);
@@ -35,7 +39,33 @@ public:
     }
 
     T getAt(int i){
+        if (i >= this-> size){
+            throw IndexOutOfBoundsException;
+        }
         return this-> items[i];
+    }
+
+    //TODO need stack first
+    void removeAll(const T &obj){
+
+//        for (int i = 0; i < this-> size; i++){
+//            if(this-> items[i].equals(obj)){
+//
+//            }
+//        }
+    }
+
+    void removeAt(int i){
+        if (i >= this-> size || i < 0) return;
+        const T obj = this-> getAt(i);
+        this-> remove(obj);
+    }
+
+    void removeRange(int min, int max){
+        if (min > max) return;
+        for (int i = min; i <= max; i++){
+            this-> removeAt(min);
+        }
     }
 
     int indexOf(const T &obj){
@@ -57,7 +87,30 @@ public:
         return index;
     }
 
+    int lastIndexOf(const T &obj, int max){
+        int index = -1;
+        if (max >= this-> size) return index;
 
+        for (int i = 0; i <= max; i++){
+            if (this-> items[i].equals(obj)){
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    int lastIndexInRangeOf(const T &obj, int min, int max){
+        int index = -1;
+        if (max >= this-> size) return index;
+        if (min > max) return index;
+
+        for (int i = min; i <= max; i++){
+            if (this-> items[i].equals(obj)){
+                index = i;
+            }
+        }
+        return index;
+    }
 
 };
 #endif //QUEUE_LIST_H
